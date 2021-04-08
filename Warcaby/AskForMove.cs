d75217a11e781fea69f5926dcff5f2cc9bec44bc
ACwojdziDC <72994305+ACwojdziDC@users.noreply.github.com>
2021-04-08 20:02:36 +0200

    start

---
+++ Warcaby/AskForMove.cs
@@ -0,0 +1,224 @@
+using System;
+using System.Linq;
+
+namespace Warcaby
+{
+    public class AskForMove
+    {
+        private readonly Messages _message = new Messages();
+        
+        
+        public (int, int)[] MakeMove(Board board, int player = 1)
+        {
+            Pawn[,] fieldsArray = board.board;
+            int boardSize = board.Size;
+            (int row, int col) pawnToMove;
+            (int, int) fieldToMoveTo;
+            (int, int)[] availableMoves = new (int, int)[] {};
+            (int, int)[] avilableCaptures = new (int, int)[] {};
+            (int, int)[] result;
+            string messageText;
+            bool isValidChoice;
+            bool isEmptyList = true;
+
+            //Player selects pawn
+            _message.WriteMessage("choosePawn");
+            do
+            {
+                pawnToMove = AskUserForCoordinates(boardSize, player);
+                isValidChoice = ValidPawn(fieldsArray, pawnToMove, player);
+                messageText = "wrongPawn";
+                if (isValidChoice)
+                {
+                    availableMoves = GetPosibleMoves(pawnToMove, player, board);
+                    //if (pawnToMove.row > 1 && pawnToMove.row < boardSize - 2)
+                    avilableCaptures = GetPosibleCaptures(pawnToMove, player, board);
+                    isEmptyList = availableMoves.Length == 0 && avilableCaptures.Length == 0;
+                    messageText = isEmptyList ? "noMove" : "whereToPlace";
+                }
+
+                _message.WriteMessage(messageText);
+            } while (!isValidChoice || isEmptyList);
+
+
+            //Player decides where to move his pawn
+            do
+            {
+                fieldToMoveTo = AskUserForCoordinates(boardSize, player);
+                isValidChoice = availableMoves.Contains(fieldToMoveTo) || avilableCaptures.Contains(fieldToMoveTo);
+                if (!isValidChoice)
+                    _message.WriteMessage("unavailableMove");
+            } while (!isValidChoice);
+
+            //Console.Out.WriteLine("Your pawn will move to {0}", fieldToMoveTo);
+
+            // move or capture + move in Game class method MakeMoveOrCapture
+            return new (int, int)[2] {pawnToMove, fieldToMoveTo};
+        }
+        
+        
+        private (int, int) AskUserForCoordinates (int boardSize = 10, int player = 1)
+        {
+            (int?, int?) pawnCoordinates;
+
+            do
+            {
+                string userInput = Console.ReadLine();
+                pawnCoordinates = ValidateInput(userInput, boardSize);
+            } while (pawnCoordinates == (null, null));
+
+            return  ConverrtNullableInts(pawnCoordinates);
+        }
+
+        
+        private (int?, int?) ValidateInput(string userInput, int boardSize)
+        {
+            int firstLetterAsciiCode = 65;
+            int numberZeroAciiCode = 49;
+            //int alphabetLength = 25;
+            int? col, row;
+
+
+            if (userInput.Length > 1 && userInput.Length < 4)
+            {
+                char[] charArr = userInput.ToUpper().ToCharArray(0, userInput.Length);
+                col = (int) charArr[0] - firstLetterAsciiCode;
+                row = userInput.Length == 2
+                    ? charArr[1] - numberZeroAciiCode
+                    : (((charArr[1] - numberZeroAciiCode + 1) * 10) + (charArr[2] - numberZeroAciiCode));
+
+                if (col >= boardSize || col < 0 || row >= boardSize || row < 0)
+                {
+                    _message.WriteMessage("outOfRange");
+                    (row, col) = (null, null);
+                }
+            }
+            else
+            {
+                _message.WriteMessage("invalidInput");
+                (row, col) = (null, null);
+            }
+
+            return (row, col);
+        }
+
+        /*
+        private (int?, int?) nullCoordinates()
+        {
+            Console.Out.WriteLine("Invalid input! Please enter correct coordinates");
+            return (null, null);
+        }
+        */
+        
+        
+        private (int, int) ConverrtNullableInts((int? rowNull, int? colNull) coordinates)
+        {
+            var colConverted = coordinates.colNull ?? default(int); //colConverted = colNull is not null ? colNum : default(int);
+            var rowConverted = coordinates.rowNull ?? default(int);
+
+            return (rowConverted, colConverted);
+        }
+        
+     
+        private bool ValidPawn(Pawn[,] board, (int row, int col) coordinates, int player = 1)
+        {
+            Pawn pawn = board[coordinates.row, coordinates.col];
+
+            if (pawn is null)
+                return false;
+            else 
+                switch (player)
+                {
+                    case 1 when pawn.IsWhite:
+                    case 2 when !pawn.IsWhite:
+                        return true;
+                    default:
+                        return false;
+                };
+        }
+        
+        
+        public (int, int)[] GetPosibleMoves((int pawnRow, int pawnCol) pawnLocation, int player, Board board)
+        {
+            (int, int)[] possibleCoordinates;
+
+            int newRow = player == 1 ? pawnLocation.pawnRow - 1 : pawnLocation.pawnRow + 1;
+            (int, int) rightMoveField = (newRow, pawnLocation.pawnCol + 1);
+            (int, int) leftMoveField = (newRow, pawnLocation.pawnCol - 1);
+
+            bool rMoveAvailable = pawnLocation.pawnCol < board.Size - 1 && isAnEmptyField(rightMoveField, board);
+            bool lMoveAvailable = pawnLocation.pawnCol > 0 && isAnEmptyField(leftMoveField, board);
+
+            if (newRow >= board.Size || newRow < 0 || (!rMoveAvailable && !lMoveAvailable))
+                return possibleCoordinates = new (int, int)[] { };
+            else if (rMoveAvailable && !lMoveAvailable)
+                return possibleCoordinates = new (int, int)[] {rightMoveField};
+            else if (!rMoveAvailable)
+                return possibleCoordinates = new (int, int)[] {leftMoveField};
+            else
+                return possibleCoordinates = new (int, int)[] {leftMoveField, rightMoveField};
+        }
+
+        
+        private bool isAnEmptyField((int row, int col) fieldCoordinates, Board board)
+        {
+            
+            if (fieldCoordinates.row < 0 || fieldCoordinates.row >= board.Size)
+                return false;
+            return (board.board[fieldCoordinates.row, fieldCoordinates.col] is null) ? true : false;
+        }
+
+        public (int, int)[] GetPosibleCaptures((int pawnRow, int pawnCol) pawnLocation, int player, Board board)
+        {
+            (int, int)[] possibleCoordinatesCapture;
+            
+            int newRow = player == 1 ? pawnLocation.pawnRow - 2 : pawnLocation.pawnRow + 2;
+            bool rowIsValid = newRow <= board.Size - 1 && newRow >= 1;
+            (int newRow, int newCol) rightMoveField = (newRow, pawnLocation.pawnCol + 2);
+            (int newRow, int newCol) leftMoveField = (newRow, pawnLocation.pawnCol - 2);
+            
+            int rowBetween = player == 1 ? pawnLocation.pawnRow - 1 : pawnLocation.pawnRow + 1;
+            (int newRow, int newCol) rightFieldBetween = (rowBetween, pawnLocation.pawnCol + 1);
+            (int newRow, int newCol) leftFieldBetween = (rowBetween, pawnLocation.pawnCol - 1);
+            
+            bool rMoveAvailable = rowIsValid && rightMoveField.newCol <= board.Size - 1 && isAnEmptyField(rightMoveField, board) && isFieldWithOppositePawn(rightFieldBetween, board, player);
+            bool lMoveAvailable = rowIsValid && leftMoveField.newCol >= 0 && isAnEmptyField(leftMoveField, board) && isFieldWithOppositePawn(leftFieldBetween, board, player);
+
+            if (newRow >= board.Size || newRow < 0 || (!rMoveAvailable && !lMoveAvailable))
+            {
+                return possibleCoordinatesCapture = new (int, int)[] { };
+            }
+            else if(rMoveAvailable && !lMoveAvailable)
+            {
+                return possibleCoordinatesCapture = new (int, int)[] {rightMoveField};
+            }
+            else if(!rMoveAvailable && lMoveAvailable)
+            {
+                return possibleCoordinatesCapture = new (int, int)[] {leftMoveField};
+            }
+            else
+            {
+                return possibleCoordinatesCapture = new (int, int)[] {leftMoveField, rightMoveField};
+            }
+        }
+
+        private bool isFieldWithOppositePawn((int row, int col) fieldCoordinate, Board board, int player)
+        {
+            if (!isAnEmptyField(fieldCoordinate, board))
+            {
+                if (player == 1)
+                {
+                    return (!board.board[fieldCoordinate.row, fieldCoordinate.col].IsWhite) ? true : false;
+                }
+                else
+                {
+                    return (board.board[fieldCoordinate.row, fieldCoordinate.col].IsWhite) ? true : false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Warcaby/Game.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Warcaby
+{
+
+    /*
+There is a class Game which contains all game logic and actions.
+There is a method Start() that starts whole game between players.
+There is a method Round() that determines one round actions i.e. check who plays next or is there a winner yet.
+There is a method that checks if starting position from user input is a valid pawn and if ending position is within board boundaries. If so, it calls TryToMakeMove() on pawn instance.
+There is a method CheckForWinner() that checks after each round is there an a winner.
+Method CheckForWinner() checks also for draws.
+    */
+    class Game
+    {
+        private Board board;
+        private bool isFinished = false;
+        private int player = 2;
+        private Player curentPlayer;
+        
+        AskForMove move = new AskForMove();
+        AI ai = new AI();
+        Menu menu = new Menu();
+        Player player1;
+        Player player2;
+
+
+        public void Start()
+        {
+            string gameMode = menu.GameMode;
+            player1 = new Player("white", 1, gameMode.Contains("HUMAN") ? true : false);
+            player2 = new Player("black", 2, gameMode.Contains("COMPUTER") ? false : true);
+            curentPlayer = player2;
+            
+            board = new Board();
+            while (!isFinished)
+            {
+                Round();
+                //Console.Clear();
+                Console.Out.WriteLine(board.ToString());
+            }
+        }
+
+        public void Round()
+        {
+            curentPlayer = curentPlayer.Equals(player2) ? player1 : player2;
+            player = player == 2 ? 1 : 2;
+            TryToMakeMove();
+            CheckForWinner();
+            int sleepTime = !curentPlayer.Human ? 1000 : 0;
+            Thread.Sleep(sleepTime);
+            //Console.ReadKey();
+        }
+
+        public void TryToMakeMove()
+        {
+            (int row, int col)[] moveCoordinates = !curentPlayer.Human ? ai.aiMove(player, board) : move.MakeMove(board, player);
+
+
+            //(int row, int col)[] moveCoordinates = move.MakeMove(board.board, board.Size, player);
+
+            if (Math.Abs(moveCoordinates[0].row - moveCoordinates[1].row) == 2 && Math.Abs(moveCoordinates[0].col - moveCoordinates[1].col) == 2)
+            {
+                (int, int) pawnToCapture;
+                if (board.board[moveCoordinates[0].row, moveCoordinates[0].col].IsWhite && (moveCoordinates[0].col - moveCoordinates[1].col == -2))
+                {
+                    pawnToCapture = (moveCoordinates[0].row - 1, moveCoordinates[0].col + 1);
+                }
+                else if (board.board[moveCoordinates[0].row, moveCoordinates[0].col].IsWhite && (moveCoordinates[0].col - moveCoordinates[1].col == 2))
+                {
+                    pawnToCapture = (moveCoordinates[0].row - 1, moveCoordinates[0].col - 1);
+                }
+                else if (!board.board[moveCoordinates[0].row, moveCoordinates[0].col].IsWhite && (moveCoordinates[0].col - moveCoordinates[1].col == -2))
+                {
+                    pawnToCapture = (moveCoordinates[0].row + 1, moveCoordinates[0].col + 1);
+                }
+                else
+                {
+                    pawnToCapture = (moveCoordinates[0].row + 1, moveCoordinates[0].col - 1);
+                }
+                board.RemovePawn(pawnToCapture);
+            }
+            board.MovePawn(moveCoordinates[0], moveCoordinates[1]);
+            
+        }
+
+        public void MakeMoveOrCapture((int row, int col) pawnCoordinate, (int row, int col) nextPawnCoordinate,
+            bool capture = false)
+        {
+            if (capture)
+            {
+                (int row, int col) pawnToCapture = (pawnCoordinate.row + 1, pawnCoordinate.col + 1);
+                board.RemovePawn(pawnToCapture);
+            }
+           
+            board.MovePawn(pawnCoordinate, nextPawnCoordinate);
+        }
+
+        public void CheckForWinner()
+        {
+            int W = 0, B = 0;
+
+            for (int row = 0; row < board.Size; row++)
+            {
+                for (int col = 0; col < board.Size; col++)
+                {
+                    Pawn p = board.board[row, col];
+                    if (board.board[row,col] != null)
+                    {
+                        if (board.board[row, col].IsWhite)
+                            W++;
+                        else
+                            B++;
+                    }
+                }
+            }
+
+            ValidateResult(W, B);
+        }
+
+        public void ValidateResult(int white, int black)
+        {
+            if (white == 1 && black == 1)
+            {
+                printEndScreen(3);
+                isFinished = true;
+            }
+            else if (white == 0)
+            {
+                printEndScreen(2);
+                isFinished = true;
+            }
+            else if (black == 0)
+            {
+                printEndScreen(1);
+                isFinished = true;
+            }
+
+            isFinished = false;
+        }
+
+        private void printEndScreen(int ending)
+        {
+            if (ending == 1)
+            {
+                Console.WriteLine("**************White has won**************");
+            }
+            else if (ending == 2)
+            {
+                Console.WriteLine("**************Black has won**************");
+            }
+            else
+            {
+                Console.WriteLine("************** Tied **************"); 
+            }
+        }
+    }
+}
